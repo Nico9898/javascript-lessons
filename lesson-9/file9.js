dccccd45536de1d05e83cf110ca63f1df59e5dbe
Nico9898 <142309748+Nico9898@users.noreply.github.com>
2023-08-27 03:19:14 +0200

    Add files via upload

---
+++ lesson-9/file9.js
@@ -0,0 +1,29 @@
+//3d task
+let name = prompt('Яка "офіційна" назва JavaScript?', '');
+
+if (name == 'ECMAScript') {
+    alert( 'True!' );
+} else {
+    alert('Ви не знаєте? ECMAScript!');
+}
+
+// 2nd task
+let value = prompt( 'Enter a number', 0);
+
+if (value > 0) {
+    alert( 1 );
+} else if (value < 0) {
+    alert( -1 );
+} else{
+    alert( 0 );
+} 
+
+// 3rd task
+
+// let message = (login == 'Працівник') ? 'Привіт' :
+// (login == 'Директор') ? 'Вітаю' :
+// (login == '') ? 'Немає логіну' :
+// '';
+
+
+
